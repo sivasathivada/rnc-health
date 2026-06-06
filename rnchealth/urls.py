7db962704d7e2df008e50429c59bfa1a325d8297
siva sathivada <sivasathivada@gmail.com>
2026-06-06 19:30:35 +0530

    setting up render base root url redirect to admin panel

--- rnchealth/urls.py
+++ rnchealth/urls.py
@@ -8,14 +8,14 @@ from django.views.generic import RedirectView   # Use this if you want to redire
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', RedirectView.as_view(url='admin/', permanent=True)),
-    
+
     path('api/auth/', include('authentication.urls')),
     path('api/v1/consultants/', include('consultants.urls')),
     path('api/v1/patients/', include('patients.urls')),
     path('api/v1/book-appointment/', include('consultations.urls')),
     path('api/v1/payments/', include('payments.urls')),
     path('api/v1/admin/', include('rnchealth.admin_urls')),
-]   path('', RedirectView.as_view(url='admin/', permanent=True)),
+    path('', RedirectView.as_view(url='admin/', permanent=True)),
 
 
 if settings.DEBUG:
